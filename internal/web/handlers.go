a3000791e80ec762199e1a403eb0e1971ba27825
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-11-20 12:22:41 +0100

    fix: accept any method

--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -10,7 +10,7 @@ import (
 // Map Server Handlers
 func (s *WebApi) MapHandlers(e *echo.Echo, prefix string) error {
 	api := e.Group(prefix)
-	api.POST("/alive/", func(c echo.Context) error {
+	api.Any("/alive/", func(c echo.Context) error {
 		mac := c.QueryParam("mac")
 		if mac == "" {
 			return c.JSON(http.StatusBadRequest, echo.Map{"error": "Missing MAC"})
@@ -18,7 +18,7 @@ func (s *WebApi) MapHandlers(e *echo.Echo, prefix string) error {
 		s.reporter.Report(mac, sensor.Alive)
 		return c.JSON(http.StatusOK, echo.Map{})
 	})
-	api.POST("/motion/", func(c echo.Context) error {
+	api.Any("/motion/", func(c echo.Context) error {
 		mac := c.QueryParam("mac")
 		if mac == "" {
 			return c.JSON(http.StatusBadRequest, echo.Map{"error": "Missing MAC"})
